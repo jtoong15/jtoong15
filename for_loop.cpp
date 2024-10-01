5058ec740338e655b6cae2e1e17fe69231ed9bb5
Justine <144922082+jtoong15@users.noreply.github.com>
2024-10-01 21:19:07 +0800

    Create for_loop.cpp

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+For loop:
+#include <iostream>
+//for loop
+int main(){
+
+// i = index
+//This is a C++ program that uses for loop to print "HAPPY NEW YEAR" five (5) times
+    for(int i = 1;  i <= 5; i++){
+        std::cout << "HAPPY NEW YEAR!\n";
+    }
+
+    return 0; //The program returns 0 to indicate successful execution.
+}
+
