b4bfaa6a7c6e5316c3d242ca1984c2ef04fd6ee9
Justine <144922082+jtoong15@users.noreply.github.com>
2024-10-01 21:33:55 +0800

    Add files via upload

---
+++ struct.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
+
+    struct addr{
+        char name[30];
+        char street[40];
+        char city[20];
+        char state[15];
+        unsigned long int zip;
+     };
+
+int main(){
+    addr my_address = {"Justine A. Toong","Atis Drive Baliwasan", "Zamboanga City" , "Philippines", 7000};
+    system("cls");
+
+    std::cout << my_address.name << " " << my_address.street << " " << my_address.city << " " << my_address.state << " " << my_address.zip;
+    system("cls");
+
+    my_address.zip = 7000;
+
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout << "\n";
+    SetConsoleTextAttribute(h, 1);
+    std::cout << strcpy(my_address.name, "Justine A. Toong");
+    SetConsoleTextAttribute(h, 14);
+    std::cout << " " << my_address.street << " " << my_address.city << " " << my_address.state << " " << my_address.zip;
+
+    return 0;
+
+}
